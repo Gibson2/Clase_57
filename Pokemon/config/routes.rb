aef9885c71af3bdad72d7615017344a7b92065a2
Guillermo <gibsonguillermoarturo@gmail.com>
2017-08-02 20:06:56 -0400

    Agregando Bootstrap

--- Pokemon/config/routes.rb
+++ Pokemon/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :pokemon_toys
+
+	get 'pokemon_toys/index'
+
+	root 'pokemon_toys#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
